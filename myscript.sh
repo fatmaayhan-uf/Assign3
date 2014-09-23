bb14dbe2d7105380f907a769499676306da81aa1
[fatmaayhan-uf] <[fayhan@ufl.edu]>
2014-09-23 19:02:24 -0400

    [change]

--- myscript.sh
+++ myscript.sh
@@ -1,3 +1,3 @@
 for planet in Mercury Venus Earth Mars Jupiter Uranus Neptun
-do curl "http://en.wikipedia.org/wiki/$planet" > $planet.txt
+do curl "http://en.wikipedia.org/wiki/$planet" > $planet.txt | git add [$planet.txt] | git commit -m "[asignment3 commit1]"
 done
